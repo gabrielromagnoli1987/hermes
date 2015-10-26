657532322c0b9595cc1f2e31bd0ba08eae4ff0bd
gabrielromagnoli1987 <gabrielromagnoli1987@gmail.com>
2015-10-26 11:20:31 -0300

    Se suben las clases del modelo

---
+++ src/model/Notificacion.java
@@ -0,0 +1,81 @@
+package model;
+
+import java.sql.Date;
+import java.util.List;
+
+public class Notificacion {
+	
+	private Integer id;
+	private String text;
+	private Date fechaEnvio;
+	private Date fechaRecepcion;
+	private Paciente paciente;
+	private Contexto contexto;
+	private List<Etiqueta> etiquetas;
+	
+	
+	public Notificacion(String text, Date fechaEnvio, Date fechaRecepcion, Paciente paciente, Contexto contexto) {
+		this.text = text;
+		this.fechaEnvio = fechaEnvio;
+		this.fechaRecepcion = fechaRecepcion;
+		this.paciente = paciente;
+		this.contexto = contexto;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getText() {
+		return text;
+	}
+	
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	public Date getFechaEnvio() {
+		return fechaEnvio;
+	}
+	
+	public void setFechaEnvio(Date fechaEnvio) {
+		this.fechaEnvio = fechaEnvio;
+	}
+	
+	public Date getFechaRecepcion() {
+		return fechaRecepcion;
+	}
+	
+	public void setFechaRecepcion(Date fechaRecepcion) {
+		this.fechaRecepcion = fechaRecepcion;
+	}
+
+	public Paciente getPaciente() {
+		return paciente;
+	}
+
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+
+	public Contexto getContexto() {
+		return contexto;
+	}
+
+	public void setContexto(Contexto contexto) {
+		this.contexto = contexto;
+	}
+
+	public List<Etiqueta> getEtiquetas() {
+		return etiquetas;
+	}
+
+	public void setEtiquetas(List<Etiqueta> etiquetas) {
+		this.etiquetas = etiquetas;
+	}
+	
+}
---
+++ src/model/Paciente.java
@@ -0,0 +1,39 @@
+package model;
+
+public class Paciente {
+	
+	private Integer id;
+	private String nombre;
+	private String apellido;
+	
+	
+	public Paciente(String nombre, String apellido) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getApellido() {
+		return apellido;
+	}
+	
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+}
