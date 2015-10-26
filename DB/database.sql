af7fbfee76f035df6eca2b4a3d48a9b177362543
gabrielromagnoli1987 <gabrielromagnoli1987@gmail.com>
2015-10-26 15:54:15 -0300

    se agrega el script de creacion de la BD y se acomodan reglas del gitignore

---
+++ DB/database.sql
@@ -0,0 +1,37 @@
+CREATE TABLE "paciente" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "nombre" TEXT NOT NULL,
+    "apellido" TEXT NOT NULL
+);
+CREATE TABLE sqlite_sequence(name,seq);
+CREATE TABLE "Etiqueta" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "nombre" TEXT NOT NULL,
+    "descripcion" TEXT
+);
+CREATE TABLE "Contexto" (
+    "id" INTEGER PRIMARY KEY NOT NULL,
+    "nombre" TEXT NOT NULL,
+    "descripcion" TEXT
+);
+CREATE TABLE "categoria" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "nombre" TEXT NOT NULL,
+    "descripcion" TEXT
+);
+CREATE TABLE "notificacion" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "text" TEXT NOT NULL,
+    "fechaEnvio" TEXT,
+    "fechaRecepcion" TEXT,
+    "pacienteID" INTEGER NOT NULL,
+    "contextoID" INTEGER NOT NULL
+);
+CREATE TABLE "etiqueta_notificacion" (
+    "etiquetaID" INTEGER NOT NULL,
+    "notificacionID" INTEGER NOT NULL
+);
+CREATE TABLE "categoria_contexto" (
+    "categoriaID" INTEGER NOT NULL,
+    "contextoID" INTEGER NOT NULL
+);
