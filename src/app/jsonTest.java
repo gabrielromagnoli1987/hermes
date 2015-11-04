2ff809d5dd1b52fc74bd8dc8771f4f258cdbe55b
gabrielromagnoli1987 <gabrielromagnoli1987@github.com>
2015-11-04 01:38:12 -0300

    se agrega la libreria gson y un test de la misma

---
+++ src/app/jsonTest.java
@@ -0,0 +1,56 @@
+package app;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import model.Categoria;
+import model.Contexto;
+import model.Notificacion;
+import model.Paciente;
+
+import com.google.gson.Gson;
+
+
+public class jsonTest {
+
+    public static void main(String[] args) {
+
+    	
+    	Paciente paciente = new Paciente("nombre 1", "apellido 1");
+    	Contexto contexto = new Contexto("el contexto", "la desc del contexto");
+    	
+    	Categoria categoria = new Categoria("cat 1", "desc cat 1");
+    	Categoria categoria2 = new Categoria("cat 2", "desc cat 2");    	
+    	List<Categoria> categorias = new ArrayList<Categoria>();
+    	categorias.add(categoria);
+    	categorias.add(categoria2);
+		contexto.setCategorias(categorias );
+    	
+    	Date fechaEnvio = new Date();
+    	Date fechaRecepcion = new Date();
+		Notificacion notificacion = new Notificacion("not 1", fechaEnvio, fechaRecepcion, paciente, contexto);
+				
+		Gson gson = new Gson();
+	
+		// convert java object to JSON format,
+		// and returned as JSON formatted string
+		String json = gson.toJson(notificacion);
+	
+		try {
+			//write converted json data to a file named "file.json"
+			FileWriter writer = new FileWriter("DB/notificacion.json");
+			writer.write(json);
+			writer.close();
+	
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	
+		System.out.println(json);
+
+    }
+
+}
