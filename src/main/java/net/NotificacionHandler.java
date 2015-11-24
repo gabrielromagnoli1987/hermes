696ebe0d03e5d5bee800145ce17c586a042cc3f0
gabrielromagnoli1987 <gabrielromagnoli1987@gmail.com>
2015-11-24 12:35:20 -0300

    se agrega la posibilidad de que el handler de notificaciones procese una lista de notificaciones en un mismo json

--- src/main/java/net/NotificacionHandler.java
+++ src/main/java/net/NotificacionHandler.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.util.List;
 
 import model.Notificacion;
 import model.dao.DAOFactory;
@@ -20,11 +21,19 @@ public class NotificacionHandler implements HttpHandler {
 	public void handle(HttpExchange arg0) throws IOException {
 		InputStream is = arg0.getRequestBody();
 		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-		
-		Notificacion notificacion = FromJSONToNotificacion.fromJSONToNotificacion(br);
-		
+
 		Storable notificacionDAO = DAOFactory.getNotificacionDAO();
-		notificacionDAO.create(notificacion);
+		
+		FromJSONToNotificacion fromJSONToNotificacion = new FromJSONToNotificacion();
+		
+//		Notificacion notificacion = fromJSONToNotificacion.fromJSONToNotificacion(br);
+//		notificacionDAO.create(notificacion);
+		
+		List<Notificacion> notificaciones = fromJSONToNotificacion.fromJSONToNotificaciones(br);
+		
+		for (Notificacion notificacion : notificaciones) {
+			notificacionDAO.create(notificacion);
+		}
 		
 		String response = "Se agrego la notificacion";
 		arg0.sendResponseHeaders(200, response.length());
--- src/main/java/utils/FromJSONToNotificacion.java
+++ src/main/java/utils/FromJSONToNotificacion.java
@@ -1,18 +1,32 @@
 package utils;
 
 import java.io.BufferedReader;
+import java.lang.reflect.Type;
+import java.util.List;
 
 import model.Notificacion;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class FromJSONToNotificacion {
 	
-	public static Notificacion fromJSONToNotificacion(BufferedReader br) {
+	public Notificacion fromJSONToNotificacion(BufferedReader br) {
 		
-		Gson gson = new Gson();		
+		Gson gson = new Gson();
 		Notificacion notificacion = gson.fromJson(br, Notificacion.class);
 		return notificacion;
+		
+	}
+	
+	public List<Notificacion> fromJSONToNotificaciones(BufferedReader br) {
+		
+		Gson gson = new Gson();		
+		
+		Type type = new TypeToken<List<Notificacion>>(){}.getType();		
+	    List<Notificacion> notificaciones = gson.fromJson(br, type);
+
+		return notificaciones;
 		
 	}
 
